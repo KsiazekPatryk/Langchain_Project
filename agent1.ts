da15ca3e0b115758ef89aaeae29d89310c974a32
Patryk Ksiazek <globalghost1@protonmail.com>
2025-12-14 19:15:37 +0100

    Agent Response_and_extract the core

--- agent1.ts
+++ agent1.ts
@@ -1,12 +1,13 @@
 import { createAgent } from "langchain";
+import "dotenv/config";
 
 const agent = createAgent(
     {model: "claude-sonnet-4-5-20250929"}
     );
 
-agent.invoke(
-    {
-        messages: [{role:"user", content:""}]
-    }
-)
+const response = await agent.invoke({
+        messages: [{role:"user", content:"What is sum of 2+2"}]
+    });
+console.log(response);
+
 
