032bff1be50c31cbd8ce024d9b175031242c3b79
Patryk Ksiazek <globalghost1@protonmail.com>
2025-12-24 20:20:04 +0100

    RecursiveTextSplitter

--- RAG/RAGagent1.ts
+++ RAG/RAGagent1.ts
@@ -4,4 +4,6 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 const loader = new PDFLoader("DOCUMENTS_PATH")
 const docs = await loader.load()
 console.log(docs.length)
+console.log(docs[0].pageContent)
+
 
