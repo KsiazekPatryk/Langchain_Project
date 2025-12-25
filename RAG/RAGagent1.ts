3d734b6dfe37a9d4f97f6d0b58ee9170d06a36bf
Patryk Ksiazek <globalghost1@protonmail.com>
2025-12-25 07:59:37 +0100

    Package_Changes

--- RAG/RAGagent1.ts
+++ RAG/RAGagent1.ts
@@ -1,4 +1,6 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 
 const loader = new PDFLoader("DOCUMENTS_PATH")
@@ -6,4 +8,15 @@ const docs = await loader.load()
 console.log(docs.length)
 console.log(docs[0].pageContent)
 
+const textSpliter = new RecursiveCharacterTextSplitter(
+    {
+    chunkSize: 1000,
+    chunkOverlap: 200
+    }
+);
+const allSplits = await textSpliter.splitDocuments(docs)
+console.log(allSplits.length);
 
+new OpenAIEmbeddings({
+    model: "text-embedding-3-large",
+})
