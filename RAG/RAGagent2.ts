bf21c0c4ea52e3161ad6a1fa253a4f822e643d7a
Patryk Ksiazek <globalghost1@protonmail.com>
2025-12-26 11:51:34 +0100

    Agent_with_RAG

--- RAG/RAGagent2.ts
+++ RAG/RAGagent2.ts
@@ -4,6 +4,7 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import {MemoryVectorStore} from "@langchain/classic/vectorstores/memory";
 import "dotenv/config";
+import { createAgent, dynamicSystemPromptMiddleware } from "langchain";
 
 const loader = new PDFLoader("DOCUMENTS_PATH")
 const docs = await loader.load()
@@ -19,13 +20,36 @@ const textSpliter = new RecursiveCharacterTextSplitter(
 const allSplits = await textSpliter.splitDocuments(docs)
 console.log(allSplits.length);
 
-new OpenAIEmbeddings({
+const embeddings = new OpenAIEmbeddings({
     model: "text-embedding-3-large",
 })
 
-const vectorStore = new MemoryVectorStore(Embeddings);
+const vectorStore = new MemoryVectorStore(embeddings);
 await vectorStore.addDocuments(allSplits);
 
+const ragMiddleware = dynamicSystemPromptMiddleware(async (state)=>
+{
+    const userMessage = state.messages[0].content;
+    const query = typeof userMessage === "string" ? userMessage : "";
+    const retrivedDocs = await vectorStore.similaritySearch(query,2 );
+    const docsContent = retrivedDocs.map((doc) => doc.pageContent).join("\n\n");
+
+    return `You are helpful assistant. Use the following context from the document to answer the user's question:\n\n${docsContent}`;
+
+
+
+
+});
+const agent = createAgent({
+    model: "gpt-4o",
+    tools: [],
+    middleware: [ragMiddleware]
+})
+
+const result = await agent.invoke({
+    messages: [{role: "user", content: "When was Nike incorporated?"}]
+
+})
 
 
 
